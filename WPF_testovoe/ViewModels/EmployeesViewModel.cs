563963af9ca1ed3e2ba9a0994f586c30cf0b520e
Lev Chuprin <leva.chuprin@mail.ru>
2020-09-04 22:38:44 +0300

    Добавлена ViewModel для сотрудников

--- WPF_testovoe/ViewModels/EmployeesViewModel.cs
+++ WPF_testovoe/ViewModels/EmployeesViewModel.cs
@@ -9,61 +9,61 @@ using WPF_testovoe.Utilty;
 
 namespace WPF_testovoe.ViewModels
 {
-    //public class EmployeesViewModel : BaseDataPageViewModel<Employee>, INotificationPageService, IViewModel
-    //{
-    //    #region Свойства
-    //    private INotification _notification;
-    //    public INotification Notification
-    //    {
-    //        get { return _notification; }
-    //        set { OnPropertyChanged(ref _notification, value); }
-    //    }
-    //    #endregion
+    public class EmployeesViewModel : BaseDataPageViewModel<Employee>, INotificationPageService, IViewModel
+    {
+        #region Свойства
+        private INotification _notification;
+        public INotification Notification
+        {
+            get { return _notification; }
+            set { OnPropertyChanged(ref _notification, value); }
+        }
+        #endregion
 
-    //    #region Конструктор
-    //    public EmployeesViewModel(ShopContext shopContext, INotification notification)
-    //        : base(shopContext)
-    //    {
-    //        Notification = notification;
-    //        NewRecord = new Employee();
-    //    }
-    //    #endregion
+        #region Конструктор
+        public EmployeesViewModel(ShopContext shopContext, INotification notification)
+            : base(shopContext)
+        {
+            Notification = notification;
+            NewRecord = new Employee();
+        }
+        #endregion
 
-    //    #region DataService
-    //    public override void DeleteRecord()
-    //    {
-    //        if (SelectedRecord != null)
-    //        {
-    //            db.Categories.Remove(SelectedRecord);
-    //            Records.Remove(SelectedRecord);
-    //            Notification.SetData(Properties.Resources.DeleteRecordSuccessfully, "Green");
-    //        }
-    //        else
-    //            Notification.SetData(Properties.Resources.RecordSelectedError, "Red");
-    //    }
-    //    public override void AddNewRecord()
-    //    {
-    //        if (String.IsNullOrEmpty(NewRecord.Name))
-    //        {
-    //            Notification.SetData(Properties.Resources.AddNewRecordError, "Red");
-    //            return;
-    //        }
+        #region DataService
+        public override void DeleteRecord()
+        {
+            if (SelectedRecord != null)
+            {
+                db.Employees.Remove(SelectedRecord);
+                Records.Remove(SelectedRecord);
+                Notification.SetData(Properties.Resources.DeleteRecordSuccessfully, "Green");
+            }
+            else
+                Notification.SetData(Properties.Resources.RecordSelectedError, "Red");
+        }
+        public override void AddNewRecord()
+        {
+            if (String.IsNullOrEmpty(NewRecord.FullName))
+            {
+                Notification.SetData(Properties.Resources.AddNewRecordError, "Red");
+                return;
+            }
 
-    //        Records.Add(NewRecord);
-    //        db.Categories.Add(NewRecord);
-    //        NewRecord = new Employee();
-    //        Notification.SetData(Properties.Resources.AddNewRecordSuccessfully, "Green");
-    //    }
-    //    public override void LoadRecords()
-    //    {
-    //        Records = new ObservableCollection<Employee>(db.Categories.ToList());
-    //        OnPropertyChanged("Categories");
-    //    }
-    //    public override void SaveAllRecords()
-    //    {
-    //        db.SaveChanges();
-    //        Notification.SetData(Properties.Resources.AllRecordsSavedSuccessfully, "Green");
-    //    }
-    //    #endregion
-    //}
+            Records.Add(NewRecord);
+            db.Employees.Add(NewRecord);
+            NewRecord = new Employee();
+            Notification.SetData(Properties.Resources.AddNewRecordSuccessfully, "Green");
+        }
+        public override void LoadRecords()
+        {
+            Records = new ObservableCollection<Employee>(db.Employees.ToList());
+            OnPropertyChanged("Categories");
+        }
+        public override void SaveAllRecords()
+        {
+            db.SaveChanges();
+            Notification.SetData(Properties.Resources.AllRecordsSavedSuccessfully, "Green");
+        }
+        #endregion
+    }
 }
