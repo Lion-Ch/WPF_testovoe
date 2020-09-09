1bd7660fb402e4443a59a1c8bb212c52dfb0ef57
Lev Chuprin <leva.chuprin@mail.ru>
2020-09-09 16:13:37 +0300

    Добавлена зависимость IUnitOfWork - EFUnitOfWork . Добавлено приложение WPF

---
+++ BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,22 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
---
+++ PL/AppViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PL
+{
+    public class AppViewModel 
+    {
+    }
+}
